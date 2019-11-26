4906693cfd4f0bf46b70b9e6c2821c69c384334a
tpinheiro1 <56200412+tpinheiro1@users.noreply.github.com>
2019-11-26 22:44:27 +0000

    Actualizar a weather app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather city="Paris" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,21 @@
 import React from "react";
-import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}ºC`
-    );
-  }
-  let apiKey = "0785a4a35a0652a43276acb84a9d4cbe";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+export default function Weather() {
+  function City() {
+      return (
+          <div className="city">Lisbon</div>
+      );
+    };
+    function Details() {
+        return (
+            <div className="row">
+                <div className="col-6">Friday 20:30</div>
+                <div className="col-6">Humidity: 80%</div>
+            </div>
+            <div className="row">
+                <div className="col-6">Clear Sky</div>
+                <div className="col-6">Wind: 2 km/h</div>
+            </div>
+            );
+    }
 }
