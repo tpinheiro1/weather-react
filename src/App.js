bbbd4a43316bdb7ad6211bb8c117a04f16c29f1e
tpinheiro1 <56200412+tpinheiro1@users.noreply.github.com>
2019-12-14 13:09:22 +0000

    Created component weather

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <small>
-        <a
-          href="https://github.com/tpinheiro1/weather-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-source code
-        </a>
-        , by Teresa Pinheiro
-      </small>
+      <div className="container">
+        <Weather />
+        <small>
+          <a
+            href="https://github.com/tpinheiro1/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Teresa Pinheiro
+        </small>
+      </div>
     </div>
   );
 }
